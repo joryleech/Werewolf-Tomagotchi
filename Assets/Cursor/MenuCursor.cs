b7b0647b3dd09b522b02fbd93d3c189c87390ab7
Jory Leech <joryleech@gmail.com>
2022-01-21 21:41:25 -0800

    Add clicking

--- Assets/Cursor/MenuCursor.cs
+++ Assets/Cursor/MenuCursor.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MenuCursor: MonoBehaviour
 {
+    SpriteRenderer sr;
+    public Sprite unclicked;
+    public Sprite clicked;
     // Start is called before the first frame update
     void Start()
     {
-       
+        sr = this.GetComponent<SpriteRenderer>();
     }
 
     private void OnGUI()
@@ -18,6 +21,15 @@ public class MenuCursor: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            sr.sprite = clicked;
+        }
+        else
+        {
+            sr.sprite = unclicked;
+           
+        }
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0;
         this.transform.position = mousePosition;
